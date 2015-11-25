a196dea3b60e7f2d0117cb9fa7816b65c325a513
JonasEndl <jonasendl1@gmail.com>
2015-11-25 10:59:07 +0100

    new

--- IT_Projekt/src/de/hdm/itprojekt/client/IT_Projekt.java
+++ IT_Projekt/src/de/hdm/itprojekt/client/IT_Projekt.java
@@ -24,6 +24,7 @@ public class IT_Projekt implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
+	 * ddddd
 	 */
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network " + "connection and try again.";
