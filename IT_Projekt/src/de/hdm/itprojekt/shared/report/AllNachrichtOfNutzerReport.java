2e2543ea7effcf89b8e7955c8087502aaacef1d7
teutagashi <tg054@hdm-stuttgart.de>
2015-12-04 14:50:48 +0100

    Klasse AllNachrichtOfNutzerReport erstellt

---
+++ IT_Projekt/src/de/hdm/itprojekt/shared/report/AllNachrichtOfNutzerReport.java
@@ -0,0 +1,9 @@
+package de.hdm.itprojekt.shared.report;
+
+import java.io.Serializable;
+
+public class AllNachrichtOfNutzerReport extends CompositeReport implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+}
