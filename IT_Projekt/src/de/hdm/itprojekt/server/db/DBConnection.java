6a7c69fc15a0c5ec335653a2581d6c73b902a52e
Georgios <gl008@hdm-stuttgart.de>
2015-12-08 17:43:48 +0100

    Klasse DBConnection hinzugefügt

---
+++ IT_Projekt/src/de/hdm/itprojekt/server/db/DBConnection.java
@@ -0,0 +1,36 @@
+package de.hdm.itprojekt.server.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import com.google.appengine.api.utils.SystemProperty;
+
+public class DBConnection {
+
+	private static Connection con = null;
+	 
+	/** URL muss noch eingefügt werden
+	   */
+	private static String googleUrl = "url";
+    private static String localUrl = "url";
+
+    public static Connection connection() {
+        if (con == null) {
+            String url = null;
+            try {
+                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+                    Class.forName("com.mysql.jdbc.GoogleDriver");
+                    url = googleUrl;
+                } else {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    url = localUrl;
+                }
+
+                con = DriverManager.getConnection(url);
+            } catch (Exception e) {
+                con = null;
+                e.printStackTrace();
+            }
+        }
+        return con;
+    }
+}
