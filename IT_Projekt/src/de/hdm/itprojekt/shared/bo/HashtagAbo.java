a4f1b7945b59235a365acb1a6736249aefe52650
JonasEndl <jonasendl1@gmail.com>
2015-12-08 14:47:06 +0100

    Fixing / Hashabo + Nachricht

--- IT_Projekt/src/de/hdm/itprojekt/shared/bo/HashtagAbo.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/bo/HashtagAbo.java
@@ -1,32 +1,6 @@
 package de.hdm.itprojekt.shared.bo;
 
-/**
- * Klasse Abonnement ist die Superklasse von HashtagAbonnement
- * @author Sarah Weimer
- *
- */
-public class HashtagAbo extends Abonnement{
-	
-	/**
-	 * Attribute eines Hashtag Abonnements
-	 */
-	private static final long serialVersionUID = 1L;
-	private int aboHashtagID;
-	private Abonnement hashtagAbo;
 
-	/**
-	 * Konstruktor
-	 */
-	public HashtagAbo() {
-	
-	}
-	
-	/**
-	 * Auslesen der abonnierten Hashtag ID
-	 * @return abonnierterHashtagID
-	 */
-	public int getAboHashtagID () {
-		return aboHashtagID;
 	}
 	
 	/**
