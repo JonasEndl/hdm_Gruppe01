a693cb658d8e1620cccb09f3d3c6e3283eb1e628
JonasEndl <jonasendl1@gmail.com>
2015-12-14 18:01:11 +0100

    Nutzer Attribute eingefügt

--- IT_Projekt/src/de/hdm/itprojekt/shared/bo/Nutzer.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/bo/Nutzer.java
@@ -1,5 +1,17 @@
 package de.hdm.itprojekt.shared.bo;
 
-public class Nutzer {
+	public class Nutzer extends BusinessObject {
 
-}
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private String Nachname;
+		private String Vorname;
+		private String Mailadresse;
+
+	
+
+
+	}
+
