c60bd457324273654c5b2085ac0b7f2c36ee5259
teutagashi <tg054@hdm-stuttgart.de>
2015-12-14 15:51:25 +0100

    Kommentare wurden hinzugefügt.

--- IT_Projekt/src/de/hdm/itprojekt/shared/report/AllAbonnementOfNutzerReport.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/report/AllAbonnementOfNutzerReport.java
@@ -1,5 +1,15 @@
 package de.hdm.itprojekt.shared.report;
 
+/**
+ * Report, der alle Abonnenten eines Nutzers darstellt.
+ * Diese Klasse enthält keine weiteren Attribute- und Methodenimplementierungen. Alles 
+ * was benötigt wird, liegt in den Superklassen vor.
+ * Dennoch wird diese Klasse benötigt um bestimmte Typen von Reports zu deklarieren um  mit
+ * ihnen objektorientiert umgehen zu können.
+ * 
+ * @author Teuta
+ *
+ */
 public class AllAbonnementOfNutzerReport extends SimpleReport {
 
 
