2e42f44669c6bc4c8fd8150eb6b256ee43304584
JonasEndl <jonasendl1@gmail.com>
2015-12-14 18:01:53 +0100

    Nutzer Methoden + Attribute geändert

--- IT_Projekt/src/de/hdm/itprojekt/shared/bo/Nutzer.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/bo/Nutzer.java
@@ -10,8 +10,34 @@ package de.hdm.itprojekt.shared.bo;
 		private String Vorname;
 		private String Mailadresse;
 
-	
+		
+			public String getNachname() {
+				return Nachname;
+			}
+
+			public void setNachname(String nachname) {
+				Nachname = nachname;
+			}
+
+			public String getVorname() {
+				return Vorname;
+			}
+
+			public void setVorname(String vorname) {
+				Vorname = vorname;
+			}
+
+			public String getMailadresse() {
+				return Mailadresse;
+			}
+
+			public void setMailadresse(String mailadresse) {
+				Mailadresse = mailadresse;
+			}
 
 
-	}
+
+		}
+
+
 
