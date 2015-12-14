475f6c72ef487208029c4e76ef4149005ee67ac5
JonasEndl <jonasendl1@gmail.com>
2015-12-14 18:07:03 +0100

    NutzerAbo angepasst

--- IT_Projekt/src/de/hdm/itprojekt/shared/bo/NutzerAbo.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/bo/NutzerAbo.java
@@ -1,5 +1,21 @@
 package de.hdm.itprojekt.shared.bo;
 
-public class NutzerAbo {
+
+public class NutzerAbo extends Abonnement {
+	
+	/**
+	 * @author Jonas Endl
+	 */
+	private static final long serialVersionUID = 1L;
+	private Nutzer nutzerAbo;
+
+	public Nutzer getNutzerAbo() {
+		return nutzerAbo;
+	}
+
+	public void setNutzerAbo(Nutzer nutzerAbo) {
+		this.nutzerAbo = nutzerAbo;
+	}
 
 }
+
