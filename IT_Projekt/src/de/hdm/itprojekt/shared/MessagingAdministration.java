067a8d1a75582f00143db9da407309c8e64ab178
ArthurZebrala <Zebrala@Zebrala-NB.Speedport_W_724V_Typ_A_05011603_00_007>
2015-12-16 01:03:36 +0100

    Anpassung

--- IT_Projekt/src/de/hdm/itprojekt/shared/MessagingAdministration.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/MessagingAdministration.java
@@ -14,39 +14,42 @@ public interface MessagingAdministration extends RemoteService {
 public void init() throws IllegalArgumentException;
 
 // Anlegen von Nutzer + Abos + Hashtag + Unterhaltung + Nachricht
-// Anpassung an NutzerAbo + HashtagAbo notwendig
+
 
 public Nutzer createNutzer(Nutzer n) throws IllegalArgumentException;
-public Abonnement createAbonnement ( Abonnement a) throws IllegalArgumentException;
+public HashtagAbo createHashtagAbo ( HashtagAbo ha) throws IllegalArgumentException;
+public NutzerAbo createNutzerAbo ( NutzerAbo na) throws IllegalArgumentException;
 public Hashtag createHashtag(Hashtag h) throws IllegalArgumentException;
 public Unterhaltung createUnterhaltung (Unterhaltung u) throws IllegalArgumentException;
 public Nachricht createNachricht( Nachricht na) throws IllegalArgumentException;
 
 /* Speichern der Nutzer + Abos + Hashtag + Unterhaltung + Nachricht
- * Anpassung an NutzerAbo + HashtagAbo notwendig!!
+
  */
 
 public void saveNutzer(Nutzer n) throws IllegalArgumentException;
 public void saveHashtag( Hashtag h) throws IllegalArgumentException;
-public void saveAbonnement( Abonnement a) throws IllegalArgumentException;
+public void saveHashtagAbo ( HashtagAbo ha) throws IllegalArgumentException;
+public void saveNutzerAbo ( NutzerAbo na) throws IllegalArgumentException;
 public void saveUnterhaltung(Unterhaltung u) throws IllegalArgumentException;
 public void saveNachricht(Nachricht na) throws IllegalArgumentException;
 
 /* Löschen der Nutzer + Abos + Hashtag + Unterhaltung + Nachricht
- * Anpassung an NutzerAbo + HashtagAbo notwendig!!
  */
 public void deleteNutzer(Nutzer n) throws IllegalArgumentException;
 public void deleteHashtag( Hashtag h) throws IllegalArgumentException;
-public void deleteAbonnement( Abonnement a) throws IllegalArgumentException;
+public void deleteHashtagAbo ( HashtagAbo ha) throws IllegalArgumentException;
+public void deleteNutzerAbo ( NutzerAbo na) throws IllegalArgumentException;
 public void deleteUnterhaltung(Unterhaltung u) throws IllegalArgumentException;
 public void deleteNachricht(Nachricht  na) throws IllegalArgumentException;
 
 /* Finden der Nutzer + Abos + Hashtag + Unterhaltung + Nachricht anhand der ID
- * Anpassung an NutzerAbo + HashtagAbo notwendig!!
+
  */
 public Nutzer getNutzerById(int id) throws IllegalArgumentException;
 public Hashtag getHashtagById(int id) throws IllegalArgumentException;
-public Abonnement getAbonnementById(int id) throws IllegalArgumentException;
+public HashtagAbo getHashtagAboById ( int id) throws IllegalArgumentException;
+public NutzerAbo getNutzerAboById (int id) throws IllegalArgumentException;
 public Unterhaltung getUnterhaltungById(int id) throws IllegalArgumentException;
 public Nachricht getNachrichtById(int id) throws IllegalArgumentException;
 
@@ -58,25 +61,36 @@ public Vector<Hashtag> findAllHashtags() throws IllegalArgumentException;
 public Vector<Nachricht> getNachrichtbyNutzer(Nutzer n)
     throws IllegalArgumentException;
 
-/*Abos anhand eines Nutzers finden
- * Anpassung NutzerAbo+ HashtagAbo
+/*NutzerAbos finden
  */
 
-public Vector<Abonnement> getAbonnementbyNutzer(Nutzer n)
+public Vector<NutzerAbo> getNutzerAbo(NutzerAbo na)
     throws IllegalArgumentException;
 
-/*Abos anhand eines Hashtags finden
- * Anpassung NutzerAbo+ HashtagAbo
+/*HashtagAbos finden
  */
 
-public Vector<Abonnement> getAbonnementbyNutzer(Hashtag h)
+public Vector<HashtagAbo> getHashtagAbo(Hashtagabo ha)
     throws IllegalArgumentException;
 
 /*Hashtags anhand eines Nutzers finden
  */
 
 public Vector<Hashtag> getHashtagbyNutzer(Nutzer n)
-    throws IllegalArgumentException;
+  
+	throws IllegalArgumentException;
+
+//Alle Nutzer
+public Vector<Nutzer>getAllNutzer() 
+		throws IllegalArgumentException;
+
+/*Alle Nutzer, die man noch abonnieren kann,
+	also noch nicht abonniert wurden.
+*/
+
+
+public Vector<Nutzer>getAllAbonnierbareNutzer()
+		throws IllegalArgumentException;
 
 }
 
