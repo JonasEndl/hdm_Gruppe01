592d6f737069ebb152d1eed7836235d9553eafbd
teutagashi <tg054@hdm-stuttgart.de>
2015-12-20 17:54:53 +0100

    Package de.hdm.itprojekt.client.gui erstellt. Klasse IT_Projekt erstellt.

---
+++ IT_Projekt/src/de/hdm/itprojekt/client/gui/IT_Projekt.java
@@ -0,0 +1,15 @@
+package de.hdm.itprojekt.client.gui;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry-Point-Klasse des Projekts <b>BankProjekt</b>.
+ */
+public class IT_Projekt implements EntryPoint {
+
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
