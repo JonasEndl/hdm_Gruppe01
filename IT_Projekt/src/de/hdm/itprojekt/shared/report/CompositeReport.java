cfac6e46357bb5528cdfc4723e813ee029fe7168
teutagashi <tg054@hdm-stuttgart.de>
2015-12-20 16:13:35 +0100

    Kommentare der Klasse CompositeReport hinzugefügt

--- IT_Projekt/src/de/hdm/itprojekt/shared/report/CompositeReport.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/report/CompositeReport.java
@@ -4,29 +4,57 @@ import java.io.Serializable;
 import java.util.*;
 
 
-/* 
- * zusammengesetzter Report. Kann aus einer Menge Teilreports bestehen.
+/**
+ * Ein zusammengesetzter Report. Kann aus einer Menge von Teil-Reports bestehen.
  */
 
 public abstract class CompositeReport extends Report implements Serializable {
 
 
   private static final long serialVersionUID = 1L;
+  
+ /**
+  * Menge der Teil-Reports.
+  */
 
 	private Vector<Report> subReports = new Vector<Report>();
 
+/**
+ * Hinzufügen eines Teil-Reports
+ * 
+ * @param r der hinzufügende Teil-Report.
+ */
 
 	public void addSubReport(Report r) {
 		this.subReports.addElement(r);
 	}
+	
+/**
+ * Entfernen eines Teil-Reports.
+ * 
+ * @param r der zu entfernende Teil-Report.
+ */
 
 	public void removeSubReport(Report r) {
 		this.subReports.removeElement(r);
 	}
 
+/**
+ * Auslesen der Anzahl von Teil-Reports.
+ * 
+ * @return int Anzahl der Teil-Reports.
+*/
 	public int getNumSubReports() {
 		return this.subReports.size();
 	}
+	
+/**
+ * Auslesen eines einzelnen Teil-Reports.
+ * 
+ * @param i Position des Teilreports. Bei n Elementen läuft der Index i von 0 bis n-1.
+ * 
+ * @return Position des Teil-Reports.
+ */
 	
 	public Report getSubReportAt(int i) {
 		return this.subReports.elementAt(i);
