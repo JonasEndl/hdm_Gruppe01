ed274a2b56130a3cc4f4d2a7f1850d566803fe26
teutagashi <tg054@hdm-stuttgart.de>
2016-01-02 00:27:48 +0100

    Klasse verbessert

--- IT_Projekt/src/de/hdm/itprojekt/client/gui/IT_Projekt.java
+++ IT_Projekt/src/de/hdm/itprojekt/client/gui/IT_Projekt.java
@@ -2,9 +2,7 @@ package de.hdm.itprojekt.client.gui;
 
 import com.google.gwt.core.client.EntryPoint;
 
-/**
- * Entry-Point-Klasse des Projekts <b>BankProjekt</b>.
- */
+
 public class IT_Projekt implements EntryPoint {
 
 	public void onModuleLoad() {
