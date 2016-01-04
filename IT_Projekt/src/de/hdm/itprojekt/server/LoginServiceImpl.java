d5068fd726cb64f720d0db706bee954fd7f1b54b
ArthurZebrala <arthurzebrala027@gmail.com>
2016-01-04 11:21:32 +0100

    LoginServiceImpl

---
+++ IT_Projekt/src/de/hdm/itprojekt/server/LoginServiceImpl.java
@@ -0,0 +1,40 @@
+package de.hdm.itprojekt.server;
+
+import com.google.appengine.api.users.User; 
+import com.google.appengine.api.users.UserService; 
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import de.hdm.itprojekt.shared.bo.Nutzer; 
+
+
+
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService{
+
+private String requestUri;
+private static final long serialVersionUID = 1L;
+
+public LoginServiceImpl()throws IllegalArgumentException { 
+} 
+
+public LoginInfo login (String requestUri) {
+
+UserService userService= UserServiceFactory.getUserService(); 
+Nutzer nutzer= userService.getCurrentUser(); 
+LoginInfo loginInfo= new LoginInfo();
+
+if (nutzer != null) { 
+loginInfo.setLoggedIn(true); 
+loginInfo.setEmailAddress(nutzer.getMailadresse()); 
+loginInfo.setVorname(nutzer.getVorname());
+loginInfo.setNachname(nutzer.getNachname()); 
+loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+
+} else { 
+loginInfo.setLoggedIn(false); 
+loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+
+}
+ return loginInfo;
+}
+}
