dfc5f07f8c9614a40919bb1bce20ed1b8cdd2525
Georgios <George.Larhanidis@hotmail.com>
2016-01-05 12:19:49 +0100

    Demo Gui aufgesetzt

---
+++ IT_Projekt/src/de/hdm/itprojekt/client/Nachrichten.java
@@ -0,0 +1,19 @@
+package de.hdm.itprojekt.client;
+
+
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class Nachrichten extends VerticalPanel {
+
+	private Button test2 = new Button("Test2");
+
+	public Nachrichten(){
+		
+		RootPanel.get("Content").add(test2);
+		
+	}
+
+}
