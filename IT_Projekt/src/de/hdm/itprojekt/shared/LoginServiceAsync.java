20dd0f8b717112a1d0cfcbbdb805422bedf68dc6
teutagashi <tg054@hdm-stuttgart.de>
2016-01-11 03:19:17 +0100

    PUBLIC HINZUGEFÜGT

--- IT_Projekt/src/de/hdm/itprojekt/shared/LoginServiceAsync.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/LoginServiceAsync.java
@@ -5,7 +5,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface LoginServiceAsync { 
 
- 	void login(String requestUri, AsyncCallback<LoginInfo> async);
+ 	public void login(String requestUri, AsyncCallback<LoginInfo> async);
   
  
  } 
