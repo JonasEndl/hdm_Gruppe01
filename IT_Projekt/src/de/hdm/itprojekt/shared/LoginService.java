0379c523333a00ba92ab714ddbb105f4ea77fd56
teutagashi <tg054@hdm-stuttgart.de>
2016-01-11 05:16:29 +0100

    @RemoteServiceRelativePath hinzugefügt.

--- IT_Projekt/src/de/hdm/itprojekt/shared/LoginService.java
+++ IT_Projekt/src/de/hdm/itprojekt/shared/LoginService.java
@@ -3,8 +3,8 @@ package de.hdm.itprojekt.shared;
 
 import com.google.gwt.user.client.rpc.RemoteService; 
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath; 
- 
- 
-public interface LoginService extends RemoteService { 
+ @RemoteServiceRelativePath("login")
+
+ public interface LoginService extends RemoteService { 
  	public LoginInfo login (String requestUri); 
  } 
