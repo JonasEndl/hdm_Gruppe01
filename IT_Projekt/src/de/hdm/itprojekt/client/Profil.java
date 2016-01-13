934524c43ce6bacbf9944fc85ada9f4f075643af
GeorgiosLarhanidis <george.larhanidis@hotmail.com>
2016-01-13 16:43:49 +0100

    Profil Gui hinzugefügt

--- IT_Projekt/src/de/hdm/itprojekt/client/Profil.java
+++ IT_Projekt/src/de/hdm/itprojekt/client/Profil.java
@@ -3,19 +3,61 @@ package de.hdm.itprojekt.client;
 import de.hdm.itprojekt.client.IT_Projekt;
 
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Label;
 
 public class Profil extends VerticalPanel {
 	Label Profilueberschrift = new Label("Profil");
-	private Button test = new Button("Test");
+	Label EMail = new Label("E-Mail: ");
+	Label VorName = new Label("Vorname: ");
+	Label NachName = new Label("Nachname: ");
+	private Button speichernEMail = new Button("speichern");
+	private Button speichernVorName = new Button("speichern");
+	private Button speichernNachName = new Button("speichern");
 	VerticalPanel contentPanel = new VerticalPanel();
-
+	HorizontalPanel EMailPanel = new HorizontalPanel();
+	HorizontalPanel VorNamePanel = new HorizontalPanel();
+	HorizontalPanel NachNamePanel = new HorizontalPanel();
+	TextBox EMailTextBox = new TextBox();
+	TextBox VorNameTextBox = new TextBox();
+	TextBox NachNameTextBox = new TextBox();
+	
 	public Profil() {
-		contentPanel.add(Profilueberschrift);
+
+		// CSS verknüpfung des contentPanels
 		contentPanel.addStyleName("contentPanel");
-		contentPanel.add(test);
+		
+		//Überschrift des Menüpunktes
+		contentPanel.add(Profilueberschrift);
+		
+		
+		// Email bearbeiten
+		EMailPanel.add(EMail);
+		EMailPanel.add(EMailTextBox);
+		EMailTextBox.setText("Beispiel@hdm-stuttgart.de");
+		contentPanel.add(EMailPanel);
+		contentPanel.add(speichernEMail);
+		
+		// Vorname bearbeiten
+		VorNamePanel.add(VorName);
+		VorNamePanel.add(VorNameTextBox);
+		VorNameTextBox.setText("Max");
+		contentPanel.add(VorNamePanel);
+		contentPanel.add(speichernVorName);
+		
+		
+		//Nachname bearbeiten
+		NachNamePanel.add(NachName);
+		NachNamePanel.add(NachNameTextBox);
+		NachNameTextBox.setText("Mustermann");
+		contentPanel.add(NachNamePanel);
+		contentPanel.add(speichernNachName);
+		
+		
+		// ContentPanel dem Content HTML Element hinzufügen
 		RootPanel.get("Content").add(contentPanel);
 
 	}
