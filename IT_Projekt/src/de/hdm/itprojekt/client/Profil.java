9a0848c73dd2e600785e66fdb7d2432a4a414e70
JonasEndl <jonasendl1@gmail.com>
2016-01-13 15:42:19 +0100

    MainPanel delete + Profil bearbeitung

--- IT_Projekt/src/de/hdm/itprojekt/client/Profil.java
+++ IT_Projekt/src/de/hdm/itprojekt/client/Profil.java
@@ -1,19 +1,23 @@
 package de.hdm.itprojekt.client;
 
+import de.hdm.itprojekt.client.IT_Projekt;
+
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
-
+import com.google.gwt.user.client.ui.Label;
 
 public class Profil extends VerticalPanel {
-
+	Label Profilueberschrift = new Label("Profil");
 	private Button test = new Button("Test");
+	VerticalPanel contentPanel = new VerticalPanel();
 
-	public Profil(){
-		
-		RootPanel.get("Content").add(test);
-		
+	public Profil() {
+		contentPanel.add(Profilueberschrift);
+		contentPanel.addStyleName("contentPanel");
+		contentPanel.add(test);
+		RootPanel.get("Content").add(contentPanel);
+
 	}
 
 }
