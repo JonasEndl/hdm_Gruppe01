ff001315b82f16e205af2f6787136716c1db923d
Georgios <George.Larhanidis@hotmail.com>
2016-01-17 22:31:47 +0100

    Grundgerüst für Nachrichten GUI

--- IT_Projekt/src/de/hdm/itprojekt/client/Nachrichten.java
+++ IT_Projekt/src/de/hdm/itprojekt/client/Nachrichten.java
@@ -4,19 +4,60 @@ import de.hdm.itprojekt.client.IT_Projekt;
 
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LayoutPanel;
 
 public class Nachrichten extends HorizontalPanel {
 
-	private Button test2 = new Button("Test2");
+	private Button MeineNachrichten = new Button("Meine Nachrichten");
+	private Button Aktualisieren = new Button("aktualisieren");
+	private Button Empfeanger = new Button("Empfeanger");
+	private Button Senden = new Button("senden");
+	private Button Sender = new Button("Sender");
+	private Button WeitereEmpfeanger = new Button("weitere Empfeanger");
+	private Button Kommentieren = new Button("kommentieren");
+	private Button Abonnieren = new Button("abonnieren");
 	HorizontalPanel contentPanel = new HorizontalPanel();
+	VerticalPanel contentPanel2 = new VerticalPanel();
+	HorizontalPanel TopButtons = new HorizontalPanel();
+	VerticalPanel NachrichterfassenContent = new VerticalPanel();
+	VerticalPanel ErhalteneNachrichtenContent = new VerticalPanel();
+	TextBox Nachrichterfassen = new TextBox();
+	HorizontalPanel NachrichtSchreibenButtons = new HorizontalPanel();
+	HorizontalPanel SenderEmpfeangerButtonContent = new HorizontalPanel();
+	HorizontalPanel KommentierenAbonnierenButtonContent = new HorizontalPanel();
+	Label Nachricht = new Label("Dies ist ein Beispiel Text. Hier würde dann die Nachricht die man erhält stehen");
 
 	public Nachrichten() {
 
-
+// Nachricht erfassen
 		contentPanel.addStyleName("contentPanel");
-		contentPanel.add(test2);
+		contentPanel.add(contentPanel2);
+		TopButtons.add(MeineNachrichten);
+		TopButtons.add(Aktualisieren);
+		contentPanel2.add(TopButtons);
+		NachrichtSchreibenButtons.add(Empfeanger);
+		NachrichtSchreibenButtons.add(Senden);
+		NachrichterfassenContent.add(Nachrichterfassen);
+		NachrichterfassenContent.add(NachrichtSchreibenButtons);
+		contentPanel2.add(NachrichterfassenContent);
+		
+// Nachricht erhalten
+		SenderEmpfeangerButtonContent.add(Sender);
+		SenderEmpfeangerButtonContent.add(WeitereEmpfeanger);
+		ErhalteneNachrichtenContent.add(SenderEmpfeangerButtonContent);
+		ErhalteneNachrichtenContent.add(Nachricht);
+		KommentierenAbonnierenButtonContent.add(Kommentieren);
+		KommentierenAbonnierenButtonContent.add(Abonnieren);
+		ErhalteneNachrichtenContent.add(KommentierenAbonnierenButtonContent);
+		
+		contentPanel2.add(ErhalteneNachrichtenContent);
+
+		
+		
 		RootPanel.get("Content").add(contentPanel);
 
 	}
